3bc9ec0f0a8c4c5582cd2b831109ab9995bde93b
brian-anderson01 <54916029+brian-anderson01@users.noreply.github.com>
2022-03-01 19:34:28 -0500

    Add files via upload

---
+++ vpndeploy.ps1
@@ -0,0 +1,20 @@
+﻿$server_address = "azuregateway-bf9c0077-ac07-4d12-a818-f8b4dadaff59-46609366b518.vpn.azure.com"
+$connection_name = "Azure VPN"
+
+$vpnadded = $null
+$vpnadded = Get-VpnConnection -name "Azure VPN" -AllUserConnection
+
+If ($vpnadded -ne $null){
+    exit
+}
+
+Else {
+    Write-Host "installing vpn"
+    [xml]$EAPXml = Get-Content -Path C:\eapconfig.xml
+
+    Add-VpnConnection -Name $connection_name -ServerAddress $server_address -TunnelType "Sstp" -EncryptionLevel "Required" -AuthenticationMethod Eap -EapConfigXmlStream $EAPXml -SplitTunneling -Force -RememberCredential -AllUserConnection
+
+    Add-VpnConnectionRoute -ConnectionName $connection_name -DestinationPrefix "192.168.100.0/23"
+    Add-VpnConnectionRoute -ConnectionName $connection_name -DestinationPrefix "192.168.50.0/23"
+    Add-VpnConnectionRoute -ConnectionName $connection_name -DestinationPrefix "52.239.170.72/32"
+}
