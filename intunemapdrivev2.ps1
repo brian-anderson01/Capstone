c8f713cdd34056bf7c5e521ad2b0ef05fa31bb25
brian-anderson01 <54916029+brian-anderson01@users.noreply.github.com>
2022-04-24 12:34:26 -0400

    Add files via upload

---
+++ intunemapdrivev2.ps1
@@ -0,0 +1,30 @@
+# Script was found here, I just replaced the sample script with my own from Azure:
+# https://yvez.be/2020/11/04/map-azure-file-share-using-intune-powershell-script/
+
+$user = whoami
+
+$ScriptDirectory = $env:APPDATA + "\Intune"
+# Check if directory already exists.
+if (!(Get-Item -Path $ScriptDirectory)) {
+    New-Item -Path $env:APPDATA -Name "Intune" -ItemType "directory"
+}
+
+# Logfile
+$ScriptLogFilePath = $ScriptDirectory + "\ConnectAzureFileShare.log"
+
+Add-Content -Path $ScriptLogFilePath -Value ((Get-Date).ToString() + ": " + "Running script as " + $user)
+
+$connectTestResult = Test-NetConnection -ComputerName azfss.file.core.windows.net -Port 445
+if ($connectTestResult.TcpTestSucceeded) {
+    cmd.exe /C "cmdkey /add:`"azfss.file.core.windows.net`" /user:`"localhost\azfss`" /pass:`"LgaZrvYKUdZGBhge1B6d2Y/8Uqi+TVLJwuCVDrSBwDCK1pxOcdtYKThU+jE3MVmGMxly3jCmboAZ+ASt5dlwIQ==`""
+    New-PSDrive -Name K -PSProvider FileSystem -Root "\\azfss.file.core.windows.net\share1" -Persist -Scope Global
+}
+
+If (Get-PSDrive -Name K) {
+    Add-Content -Path $ScriptLogFilePath -Value ((Get-Date).ToString() + ": " + "K-Drive mapped successfully.")
+}
+
+Else {
+    Add-Content -Path $ScriptLogFilePath -Value ((Get-Date).ToString() + ": " + "Please verify installation.")
+}
+pause
